d23de760fb9c01b2f7cfd71d81f29a6e4605c53b
Uchqun7040 <89504551+Uchqun7040@users.noreply.github.com>
2022-06-24 03:18:54 +0500

    tayyor

---
+++ dictionary/dictionary/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace dictionary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.til = new System.Windows.Forms.Label();
+            this.asosiy = new System.Windows.Forms.Label();
+            this.uqilishi = new System.Windows.Forms.Label();
+            this.tarjima = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.linklabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // til
+            // 
+            this.til.AutoSize = true;
+            this.til.BackColor = System.Drawing.Color.LimeGreen;
+            this.til.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.til.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.til.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.til.ForeColor = System.Drawing.Color.White;
+            this.til.Location = new System.Drawing.Point(288, 23);
+            this.til.Name = "til";
+            this.til.Padding = new System.Windows.Forms.Padding(3, 1, 3, 3);
+            this.til.Size = new System.Drawing.Size(127, 37);
+            this.til.TabIndex = 0;
+            this.til.Text = "English";
+            this.til.Click += new System.EventHandler(this.til_Click);
+            // 
+            // asosiy
+            // 
+            this.asosiy.AutoSize = true;
+            this.asosiy.BackColor = System.Drawing.Color.Gainsboro;
+            this.asosiy.Font = new System.Drawing.Font("Georgia", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.asosiy.ForeColor = System.Drawing.Color.Black;
+            this.asosiy.Location = new System.Drawing.Point(300, 101);
+            this.asosiy.Name = "asosiy";
+            this.asosiy.Padding = new System.Windows.Forms.Padding(3);
+            this.asosiy.Size = new System.Drawing.Size(106, 37);
+            this.asosiy.TabIndex = 0;
+            this.asosiy.Text = "asosiy";
+            this.asosiy.Resize += new System.EventHandler(this.asosiy_Resize);
+            // 
+            // uqilishi
+            // 
+            this.uqilishi.AutoSize = true;
+            this.uqilishi.BackColor = System.Drawing.Color.LightGray;
+            this.uqilishi.Font = new System.Drawing.Font("Georgia", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.uqilishi.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.uqilishi.Location = new System.Drawing.Point(295, 235);
+            this.uqilishi.Margin = new System.Windows.Forms.Padding(3);
+            this.uqilishi.Name = "uqilishi";
+            this.uqilishi.Padding = new System.Windows.Forms.Padding(3);
+            this.uqilishi.Size = new System.Drawing.Size(112, 35);
+            this.uqilishi.TabIndex = 0;
+            this.uqilishi.Text = "uqilishi";
+            this.uqilishi.Resize += new System.EventHandler(this.asosiy_Resize);
+            // 
+            // tarjima
+            // 
+            this.tarjima.AutoSize = true;
+            this.tarjima.BackColor = System.Drawing.Color.White;
+            this.tarjima.Font = new System.Drawing.Font("Georgia", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tarjima.ForeColor = System.Drawing.Color.Black;
+            this.tarjima.Location = new System.Drawing.Point(290, 168);
+            this.tarjima.Name = "tarjima";
+            this.tarjima.Padding = new System.Windows.Forms.Padding(3);
+            this.tarjima.Size = new System.Drawing.Size(123, 37);
+            this.tarjima.TabIndex = 0;
+            this.tarjima.Text = "tarjima";
+            this.tarjima.Visible = false;
+            this.tarjima.Resize += new System.EventHandler(this.asosiy_Resize);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 2000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // linklabel
+            // 
+            this.linklabel.AutoSize = true;
+            this.linklabel.BackColor = System.Drawing.Color.LightGray;
+            this.linklabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.linklabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.linklabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.linklabel.ForeColor = System.Drawing.Color.Black;
+            this.linklabel.Location = new System.Drawing.Point(19, 344);
+            this.linklabel.Name = "linklabel";
+            this.linklabel.Size = new System.Drawing.Size(87, 19);
+            this.linklabel.TabIndex = 1;
+            this.linklabel.Text = "Fayl tanlang";
+            this.linklabel.Click += new System.EventHandler(this.linklabel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(19, 324);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "(1)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(172, 336);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.SystemColors.Info;
+            this.numericUpDown1.Location = new System.Drawing.Point(51, 324);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
+            this.numericUpDown1.Size = new System.Drawing.Size(39, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.numericUpDown1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.BackgroundImage = global::dictionary.Properties.Resources.back2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(679, 378);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linklabel);
+            this.Controls.Add(this.uqilishi);
+            this.Controls.Add(this.tarjima);
+            this.Controls.Add(this.asosiy);
+            this.Controls.Add(this.til);
+            this.ForeColor = System.Drawing.Color.DarkBlue;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Dictionary";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label til;
+        private System.Windows.Forms.Label asosiy;
+        private System.Windows.Forms.Label uqilishi;
+        private System.Windows.Forms.Label tarjima;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label linklabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ dictionary/dictionary/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using iTextSharp.text;
+using System.Windows.Forms;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace dictionary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool paz = false;
+
+
+        TreeNode uz0 = new TreeNode();
+        TreeNode en0 = new TreeNode();
+        TreeNode uq0 = new TreeNode();
+        TreeNode sanoq = new TreeNode();
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar==' ')
+            {
+                tarjima.Visible = !tarjima.Visible;
+                if (til.Text == "O'zbekcha")
+                {
+                    uqilishi.Visible = !uqilishi.Visible;
+                }
+                timer.Enabled = !timer.Enabled;
+                paz = !paz;
+                if (!paz)
+                {
+                    til_almashsa();
+                }
+            }
+            else
+            {
+                if (til.Text == "O'zbekcha")
+                {
+                    til.Text = "English";
+                }
+                else
+                {
+                    til.Text = "O'zbekcha";
+                }
+                til_almashsa();
+                
+            }
+
+        }
+
+
+        public void til_almashsa()
+        {
+            if (!paz)
+            {
+
+                if (til.Text == "O'zbekcha")
+                {
+                    uqilishi.Visible = false;
+
+                }
+                else
+                {
+                    tarjima.Visible = false;
+                    uqilishi.Visible = true;
+                }
+                
+            }
+            qiymat_kirit();
+        }
+
+        public void qiymat_kirit()
+        {
+            if (linklabel.Text != "Fayl tanlang")
+            {
+                if (til.Text == "O'zbekcha")
+                {
+                    asosiy.Text = uz0.Nodes[value].Text;
+                    tarjima.Text = en0.Nodes[value].Text;
+                }
+                else
+                {
+                    asosiy.Text = en0.Nodes[value].Text;
+                    tarjima.Text = uz0.Nodes[value].Text;
+                }
+                uqilishi.Text = uq0.Nodes[value].Text;
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //til_almashsa();
+           
+
+
+        }
+        int value = 0;
+        Random r = new Random();
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (sanoq.Nodes.Count>0)
+            {
+                
+                
+                value = r.Next(0, sanoq.Nodes.Count - 1);
+
+                int v = value;
+                value = Convert.ToInt16(sanoq.Nodes[v].Text);
+                sanoq.Nodes[v].Remove();
+                qiymat_kirit();
+
+            }
+            else
+            {
+
+                if (label1.Text == "∞")
+                {
+                    int len = uz0.Nodes.Count;
+                    for (int i = 0; i < len; i++)
+                    {
+                        sanoq.Nodes.Add(i.ToString());
+                    }
+
+                }
+
+                else
+                {
+                    linklabel.Text = "Fayl tanlang";
+                    timer.Stop();
+                }
+                
+            }
+            
+        }
+
+        private void til_Click(object sender, EventArgs e)
+        {
+            if (til.Text == "O'zbekcha")
+            {
+                til.Text = "English";
+            }
+            else
+            {
+                til.Text = "O'zbekcha";
+            }
+            til_almashsa();
+        }
+
+
+        public static string pdfText(string path)
+        {
+            PdfReader reader = new PdfReader(path);
+            string text = string.Empty;
+            for (int page = 1; page <= reader.NumberOfPages; page++)
+            {
+                text += PdfTextExtractor.GetTextFromPage(reader, page);
+            }
+            reader.Close();
+            return text;
+        }
+        private void linklabel_Click(object sender, EventArgs e)
+        {
+            DialogResult d = openFileDialog.ShowDialog();
+            if (d == DialogResult.OK)
+            {
+                
+                StreamReader baza = new StreamReader(openFileDialog.FileName);
+                string[] bo = baza.ReadLine().Split(';');
+
+                if (bo.Length != 3)
+                {
+                    MessageBox.Show("Faylning birinchi qatorida ustun haqidagi malumotlar bo'lishi kerak masalan uz,en,uq");
+                }
+                else
+                {
+                    int uz = 0, en = 0, uq = 0;
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (bo[i] == "uz")
+                        {
+                            uz = i;
+                        }
+                        else if (bo[i] == "en")
+                        {
+                            en = i;
+
+                        }
+                        else
+                        {
+                            uq = i;
+                        }
+                    }
+                    string s = baza.ReadLine();
+                    while (s != null)
+                    {
+                        bo = s.Split(';');
+                        if (bo.Length > 2)
+                        {
+                            uz0.Nodes.Add(bo[uz]);
+                            en0.Nodes.Add(bo[en]);
+                            uq0.Nodes.Add(bo[uq]);
+                        }
+
+                        s = baza.ReadLine();
+                    }
+
+                    int len = uz0.Nodes.Count;
+                    for(int i = 0; i < len; i++)
+                    {
+                        string t = i.ToString();
+                        sanoq.Nodes.Add(t);
+                    }
+
+                    timer.Start();
+                    linklabel.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "(1)")
+            {
+                label1.Text = "∞";
+            }
+            else
+            {
+                label1.Text = "(1)";
+            }
+        }
+
+        private void asosiy_Resize(object sender, EventArgs e)
+        {
+            int x = ClientSize.Width;
+            int y = ClientSize.Height;
+
+            //textBox1.Size = new Size(x * 664 / 704, y * 41 / 442);
+            //textBox1.Location = new Point(x * 12 / 704, y * 62 / 442);
+            
+            int l1 = asosiy.Size.Width;
+            int l2 = tarjima.Size.Width;
+            int l3 = uqilishi.Size.Width;
+            int t = til.Size.Width;
+            asosiy.Location = new Point((x - l1) / 2, asosiy.Location.Y);
+            tarjima.Location = new Point((x - l2) / 2, tarjima.Location.Y);
+            uqilishi.Location = new Point((x - l3) / 2, uqilishi.Location.Y);
+            til.Location = new Point((x - t) / 2, til.Size.Height);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            if (numericUpDown1.Value > 0)
+                timer.Interval = (int)numericUpDown1.Value * 1000;
+            else
+            {
+                timer.Interval = 1000;
+                numericUpDown1.Value = 1;
+            }
+        }
+        
+    }
+}
